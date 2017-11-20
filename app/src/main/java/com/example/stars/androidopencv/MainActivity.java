bd794fed5dcf3657d0e5d7ef9785129a9f0ccef9
Saranya-Rajagopalan <starsaran.ur@gmail.com>
2017-11-20 02:17:55 -0700

    Remove unwanted files. Changed the buttons to radio button

--- app/src/main/java/com/example/stars/androidopencv/MainActivity.java
+++ app/src/main/java/com/example/stars/androidopencv/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.stars.androidopencv;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +26,9 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -31,21 +37,32 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements OnTouchListener, CvCameraViewListener2 {
     private static final int REQUEST_CODE = 100;
     private static final Scalar RED = new Scalar ( 255, 0, 0 );
     private static final Scalar GREEN = new Scalar ( 0, 255, 0 );
     private static final Scalar BLUE = new Scalar ( 0, 0, 255 );
-    private TextView touch_coordinates, touch_color;
-    private Button red_button, green_button, blue_button, pick_button;
-    private Switch highlight_button;
+    private static final Scalar YELLOW = new Scalar ( 255, 255, 0, 255 );
+    private static final Scalar MAGENTA = new Scalar ( 255, 0, 255, 255 );
+    private static final Scalar CYAN = new Scalar ( 0, 255, 255, 255 );
+    private static final Scalar WHITE = new Scalar ( 255, 255, 255, 255 );
+    private static Scalar highlight_mask = WHITE;
     int color = -1;
+    private TextView touch_coordinates, touch_color;
+    private Button save_button;
+    private Switch highlight_button, show_object;
     private CameraBridgeViewBase mOpenCvCameraView;
     private Mat mRgba;
     private Scalar mBlobColorRgba;
     private Scalar mBlobColorHsv;
 
-    private boolean highlight = false;
+    private boolean save = false;
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback ( this ) {
         @Override
         public void onManagerConnected(int status) {
@@ -77,49 +94,36 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById ( R.id.opencv_tutorial_activity_surface_view );
         touch_coordinates = (TextView) findViewById ( R.id.touch_coordinates );
         touch_color = (TextView) findViewById ( R.id.touch_color );
-        red_button = (Button) findViewById ( R.id.red_button );
-        green_button = (Button) findViewById ( R.id.green_button );
-        blue_button = (Button) findViewById ( R.id.blue_button );
-        pick_button = (Button) findViewById ( R.id.pick_button );
+        save_button = (Button) findViewById ( R.id.save_button );
         highlight_button = (Switch) findViewById ( R.id.highlight_button );
-        red_button.setOnClickListener ( new View.OnClickListener () {
+        show_object = (Switch) findViewById ( R.id.show_object );
+
+        save_button.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                color = 0;
-                Toast.makeText ( getApplicationContext (), "Red clicked", Toast.LENGTH_SHORT ).show ();
-            }
-        } );
-        green_button.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View view) {
-                color = 1;
-                Toast.makeText ( getApplicationContext (), "Green clicked", Toast.LENGTH_SHORT ).show ();
-            }
-        } );
-        blue_button.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View view) {
-                color = 2;
-                Toast.makeText ( getApplicationContext (), "Blue clicked", Toast.LENGTH_SHORT ).show ();
-            }
-        } );
-        pick_button.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View view) {
-                color = 3;
-                Toast.makeText ( getApplicationContext (), "Alright! Pick a color from the scene!",
-                        Toast.LENGTH_SHORT ).show ();
+                save = true;
+                Toast.makeText ( getApplicationContext (), "Saving..", Toast.LENGTH_SHORT ).show ();
             }
         } );
         highlight_button.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                highlight = !highlight;
-                if(highlight)
-                    Toast.makeText ( getApplicationContext (), "Highlighting!",
-                        Toast.LENGTH_SHORT ).show ();
+                if (highlight_button.isChecked ())
+                    Toast.makeText ( getApplicationContext (), "Highlighting color",
+                            Toast.LENGTH_SHORT ).show ();
                 else
                     Toast.makeText ( getApplicationContext (), "Not highlighting!",
+                            Toast.LENGTH_SHORT ).show ();
+            }
+        } );
+        show_object.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                if (show_object.isChecked ())
+                    Toast.makeText ( getApplicationContext (), "Objects boxed",
+                            Toast.LENGTH_SHORT ).show ();
+                else
+                    Toast.makeText ( getApplicationContext (), "Objects not displayed",
                             Toast.LENGTH_SHORT ).show ();
             }
         } );
@@ -219,6 +223,61 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
         mBlobColorHsv = new Scalar ( 255 );
     }
 
+    public void onFilterRadioClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked ();
+
+        // Check which radio button was clicked
+        switch (view.getId ()) {
+            case R.id.yellow:
+                if (checked)
+                    highlight_mask = YELLOW;
+                break;
+            case R.id.cyan:
+                if (checked)
+                    highlight_mask = CYAN;
+                break;
+            case R.id.magenta:
+                if (checked)
+                    highlight_mask = MAGENTA;
+                break;
+            case R.id.none:
+                if (checked)
+                    highlight_mask = WHITE;
+                break;
+        }
+    }
+
+    public void onHighlightRadioClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked ();
+
+        // Check which radio button was clicked
+        switch (view.getId ()) {
+            case R.id.red:
+                if (checked) {
+                    color = 0;
+                    Toast.makeText ( getApplicationContext (), "Red selected", Toast.LENGTH_SHORT ).show ();
+                }
+                break;
+            case R.id.green:
+                if (checked) {
+                    color = 1;
+                    Toast.makeText ( getApplicationContext (), "Green selected", Toast.LENGTH_SHORT ).show ();
+                }
+                break;
+            case R.id.blue:
+                if (checked) {
+                    color = 2;
+                    Toast.makeText ( getApplicationContext (), "Blue selected", Toast.LENGTH_SHORT ).show ();
+                }
+                break;
+            case R.id.none_0:
+                if (checked)
+                    color = -1;
+                break;
+        }
+    }
+
+
     @Override
     public void onCameraViewStopped() {
         mRgba.release ();
@@ -227,15 +286,14 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
     public Mat detectColor() {
 
         Mat struct_element = Imgproc.getStructuringElement ( Imgproc.MORPH_RECT, new Size ( 5, 5 ) );
-        Mat labels = new Mat (), stats = new Mat (), centroids = new Mat ();
-
         Mat inputHSV = new Mat ();
-        Mat mask1 = new Mat ();
-        Mat mask2 = new Mat ();
-        Scalar boxColor;
+
+        Scalar boxColor = new Scalar ( 0, 0, 0 );
         Imgproc.cvtColor ( mRgba, inputHSV, Imgproc.COLOR_RGB2HSV );
         switch (color) {
             case 0:
+                Mat mask1 = new Mat ();
+                Mat mask2 = new Mat ();
                 Core.inRange ( inputHSV, new Scalar ( 0, 190, 190 ),
                         new Scalar ( 15, 255, 255 ), mask1 );
                 Core.inRange ( inputHSV, new Scalar ( 160, 190, 190 ),
@@ -247,7 +305,7 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
                 break;
 
             case 1:
-                Core.inRange ( inputHSV, new Scalar ( 30, 70, 100 ),
+                Core.inRange ( inputHSV, new Scalar ( 45, 70, 100 ),
                         new Scalar ( 90, 255, 255 ), inputHSV );
                 boxColor = GREEN;
                 break;
@@ -256,84 +314,114 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
                         new Scalar ( 138, 255, 255 ), inputHSV );
                 boxColor = BLUE;
                 break;
-            case 3:
-                int h = (int) mBlobColorHsv.val[0];
-                int s = (int) mBlobColorHsv.val[1];
-                int v = (int) mBlobColorHsv.val[2];
-                Core.inRange ( inputHSV, new Scalar ( h-20, s-10, v-10 ),
-                        new Scalar ( h+40, 255, 255 ), inputHSV );
-                boxColor = convertScalarHsv2Rgba ( new Scalar ( h,s,v ) );
-                break;
-            default:
-                return mRgba;
-        }
+            case -1:
 
-        if(highlight){
-            //https://docs.opencv.org/3.2.0/d0/d86/tutorial_py_image_arithmetics.html
-            Mat mask = inputHSV;
-            Mat inv_mask = new Mat();
-            Core.bitwise_not (mask, inv_mask);
-            Mat mGray = new Mat();
-            Imgproc.cvtColor ( mRgba, mGray, Imgproc.COLOR_RGB2GRAY );
-            Imgproc.cvtColor ( mGray, mGray, Imgproc.COLOR_GRAY2RGBA);
-            Mat background = new Mat();
-            Mat foreground = new Mat();
-            Core.bitwise_and ( mGray, mGray, background, inv_mask );
-            Core.bitwise_and ( mRgba, mRgba, foreground, mask );
-            Core.add ( background, foreground, mRgba );
-
-            mask.release ();
-            inv_mask.release ();
-            mGray.release ();
-            background.release ();
-            foreground.release ();
         }
 
 
+        if (color >= 0) {
+            if (highlight_button.isChecked () || highlight_mask != WHITE) {
+                //https://docs.opencv.org/3.2.0/d0/d86/tutorial_py_image_arithmetics.html
+                Mat mask = new Mat ();
+                inputHSV.copyTo ( mask );//mask obj in white
+                Mat inv_mask = new Mat ();
+                Core.bitwise_not ( mask, inv_mask );//obj in black
+                Mat original_background = new Mat ();
+                if (highlight_button.isChecked ()) {
+                    Imgproc.cvtColor ( mRgba, original_background, Imgproc.COLOR_RGB2GRAY );  //gray original image
+                    Imgproc.cvtColor ( original_background, original_background, Imgproc.COLOR_GRAY2RGBA );  //change depth to 4
+                } else {
+                    mRgba.copyTo ( original_background );
+                }
+                Mat background = new Mat ();
+                Mat foreground = new Mat ();
 
-        /*
-        if (highlight){
-            for(int i = 0; i <inputHSV.rows (); i++)
-                for(int j = 0; j <inputHSV.cols(); j++){
-                    if(inputHSV.get (i, j )[0]==1){
-                        double[] value = mRgba.get (i,j);
-                        double average = (value[0] + value[1] + value[2])/3;
-                        value[0] =  average;
-                        value[1] =  average;
-                        value[2] =  average;
-                        mRgba.put(i ,j ,value);
+                Core.bitwise_and ( original_background, original_background, background, inv_mask );
+
+                //add_filter
+                if (highlight_mask != WHITE)
+                    mRgba.setTo ( highlight_mask, mask );
+                Core.bitwise_and ( mRgba, mRgba, foreground, mask );
+                Core.add ( background, foreground, mRgba );
+
+                mask.release ();
+                inv_mask.release ();
+                original_background.release ();
+                background.release ();
+                foreground.release ();
+            }
+
+            if (show_object.isChecked ()) {
+                Mat labels = new Mat (), stats = new Mat (), centroids = new Mat ();
+
+                //Opening and Closing Morphological operation
+                Imgproc.erode ( inputHSV, inputHSV, struct_element );
+                Imgproc.dilate ( inputHSV, inputHSV, struct_element );
+                Imgproc.dilate ( inputHSV, inputHSV, struct_element );
+                Imgproc.erode ( inputHSV, inputHSV, struct_element );
+                //http://answers.opencv.org/question/96443/find-rectangle-from-image-in-android/
+                int n = Imgproc.connectedComponentsWithStats ( inputHSV, labels, stats, centroids );
+
+                Imgproc.cvtColor ( inputHSV, inputHSV, Imgproc.COLOR_GRAY2RGB );
+
+                for (int i = 0; i < n; i++) {
+                    double[] left = stats.get ( i, Imgproc.CC_STAT_LEFT );
+                    double[] top = stats.get ( i, Imgproc.CC_STAT_TOP );
+                    double[] width = stats.get ( i, Imgproc.CC_STAT_WIDTH );
+                    double[] height = stats.get ( i, Imgproc.CC_STAT_HEIGHT );
+                    double[] area = stats.get ( i, Imgproc.CC_STAT_AREA );
+                    //outer rect area = 414720
+                    if ((area[0] < 200000) && (300 < area[0]))
+                        Imgproc.rectangle ( mRgba, new Point ( left[0], top[0] ),
+                                new Point ( left[0] + width[0], top[0] + height[0] ), boxColor, 5 );
+                }
+                labels.release ();
+                centroids.release ();
+                stats.release ();
+            }
+        }
+
+        if (save) {
+            Bitmap temp = null;
+            save = false;
+            try {
+                temp = Bitmap.createBitmap ( mRgba.cols (), mRgba.rows (), Bitmap.Config.ARGB_8888 );
+                Utils.matToBitmap ( mRgba, temp );
+            } catch (CvException e) {
+                e.printStackTrace ();
+            }
+
+            FileOutputStream fp = null;
+            String filename = new SimpleDateFormat ("yyyyMMddHHmmssSSS'.png'").format(new Date ());
+
+            File sd = new File ( Environment.getExternalStorageDirectory () + "/MyApplication" );
+            boolean success = true;
+            if (!sd.exists ()) {
+                success = sd.mkdir ();
+            }
+
+            if (success) {
+                File dest = new File ( sd, filename );
+                try {
+                    fp = new FileOutputStream ( dest );
+                    temp.compress ( Bitmap.CompressFormat.PNG, 100, fp );
+                } catch (Exception e) {
+                    e.printStackTrace ();
+                } finally {
+                    try {
+                        if (fp != null) {
+                            fp.close ();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace ();
                     }
                 }
-        }
-        */
+            }
 
-        //Opening and Closing Morphological operation
-        Imgproc.erode ( inputHSV, inputHSV, struct_element );
-        Imgproc.dilate ( inputHSV, inputHSV, struct_element );
-        Imgproc.dilate ( inputHSV, inputHSV, struct_element );
-        Imgproc.erode ( inputHSV, inputHSV, struct_element );
-
-        //http://answers.opencv.org/question/96443/find-rectangle-from-image-in-android/
-        int n = Imgproc.connectedComponentsWithStats ( inputHSV, labels, stats, centroids );
-
-        Imgproc.cvtColor ( inputHSV, inputHSV, Imgproc.COLOR_GRAY2RGB );
-
-        for (int i = 0; i < n; i++) {
-            double[] left = stats.get ( i, Imgproc.CC_STAT_LEFT );
-            double[] top = stats.get ( i, Imgproc.CC_STAT_TOP );
-            double[] width = stats.get ( i, Imgproc.CC_STAT_WIDTH );
-            double[] height = stats.get ( i, Imgproc.CC_STAT_HEIGHT );
-            double[] area = stats.get ( i, Imgproc.CC_STAT_AREA );
-            //outer rect area = 414720
-            if ((area[0] < 200000) && (300 < area[0]))
-                Imgproc.rectangle ( mRgba, new Point ( left[0], top[0] ),
-                        new Point ( left[0] + width[0], top[0] + height[0] ), boxColor, 5 );
         }
 
         inputHSV.release ();
-        labels.release ();
-        centroids.release ();
-        stats.release ();
+
         return mRgba;
     }
 
@@ -356,4 +444,3 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener, 
 
     }
 }
-
